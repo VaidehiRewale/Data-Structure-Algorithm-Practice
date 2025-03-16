4dfd63e297ebb9726ad80c96f11b8192b343682f
VaidehiRewale <139336081+VaidehiRewale@users.noreply.github.com>
2025-03-16 21:44:28 +0530

    Create Problem11.py

---
+++ Problem11.py
@@ -0,0 +1,8 @@
+Implement a function that checks if a given string is a palindrome. A palindrome is a word, or a sequence of characters that reads the same forward and backward. 
+
+String = input("Enter a string:")
+if String == String [::-1]:
+    print(f"{String} is a palindrome:")
+else:
+    print(f"{String} is not a palindrome:")
+
